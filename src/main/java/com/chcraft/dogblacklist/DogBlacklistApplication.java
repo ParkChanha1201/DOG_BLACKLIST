9f96fbfd1f23335f7f6a01403355127bc484b7da
박찬하 <chanhacraft@naver.com>
2021-05-10 12:41:21 +0900

    first commit

---
+++ src/main/java/com/chcraft/dogblacklist/DogBlacklistApplication.java
@@ -0,0 +1,13 @@
+package com.chcraft.dogblacklist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogBlacklistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogBlacklistApplication.class, args);
+	}
+
+}
