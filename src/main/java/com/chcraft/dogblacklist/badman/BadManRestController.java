d3a571f8b0b66a8543425123f08afb2123261819
박찬하 <chanhacraft@naver.com>
2021-05-11 15:22:29 +0900

    .

--- src/main/java/com/chcraft/dogblacklist/badman/BadManRestController.java
+++ src/main/java/com/chcraft/dogblacklist/badman/BadManRestController.java
@@ -2,6 +2,7 @@ package com.chcraft.dogblacklist.badman;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,8 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("badman")
 public class BadManRestController {
+	@Autowired
+	BadManService badManService;
+
 	@GetMapping("list")
-	public List<BadMan> getAll(){
-		return null;
+	public List<BadManDTO> getAll(){
+		List<BadManDTO> badManList = badManService.getAll();
+
+		return badManList;
 	}
 }
